5b319c14132a9c8bad9bcc1a0d492261892ad8ad
Raymond Overfield <Raymond.Overfield88@gmail.com>
2023-10-19 16:00:18 +0300

    adding text and logo works

--- main.py
+++ main.py
@@ -3,23 +3,82 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 from PIL import ImageTk, Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import os
 
 
 root = Tk()
 root.title("Watermark your photo")
 root.geometry("1000x900")
-root.grid_rowconfigure(1, weight=1) # Allow row 0 to expand
-root.grid_columnconfigure(0, weight=1) # Allow column 0 to expand
+root.grid_rowconfigure(3, weight=3) # Allow row 3 to expand
+root.grid_columnconfigure(0, weight=3) # Allow column 0 to expand
 
 mainframe = ttk.Frame(root)
 mainframe.grid(column=0, row=0, padx=10, pady=10, sticky=(N, W, E, S))
 
-filepath_var = StringVar(master=None)
-filepath_var.set("No file selected")
+img_filepath_var = StringVar(master=None)
+img_filepath_var.set("No file selected")
+img_filepath = ""
+
+logo_filepath_var = StringVar(master=None)
+logo_filepath_var.set("No file selected")
+logo_filepath = ""
 
 # Create a label for displaying the image
 image_label = ttk.Label(root)
-image_label.grid(column=0, row=1, columnspan=4, pady=10)
+image_label.grid(column=0, row=3, columnspan=4, pady=10)
+
+def return_file_name(filepath):
+    # Use os.path to split the file path
+    file_dir, file_name = os.path.split(filepath)
+
+    # Split the file name from its extension
+    file_name_without_extension, file_extension = os.path.splitext(file_name)
+    return file_name_without_extension, file_extension
+
+def add_logo_on_image():
+    global img_filepath
+    # Open image that is currently visible (in case it is open) and make a copy of it
+    image = Image.open(img_filepath)
+    wm_logo_image = image.copy()
+    width, height = wm_logo_image.size
+    x = width - 200
+    # Open logo image
+    logo_image = Image.open(logo_filepath)
+    logo_image = logo_image.resize((200,200))
+    #print(logo_image.size)
+    wm_logo_image.paste(logo_image, (x, 0), logo_image)#, 200, 200))
+    # logo_image.close()
+    # wm_logo_image.close()
+    # image.close()
+
+    logo_name, logo_extension = return_file_name(img_filepath)
+
+    wm_img_logo_path = "images_with_logo/" + logo_name + logo_extension
+    wm_logo_image.save(wm_img_logo_path)
+    open_image(wm_img_logo_path)
+
+
+def add_text_on_image():
+    global img_filepath
+    font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/Ubuntu-M.ttf", 40)
+    image = Image.open(img_filepath)
+
+    print(img_filepath)
+    watermark_image = image.copy()
+    draw = ImageDraw.Draw(watermark_image)
+    width, height = watermark_image.size
+    wm_text = text_input.get()
+    draw.text((width/2, 50), wm_text, fill=(255, 255, 255), font=font, anchor="ms")
+    #watermark_image.show()
+    img_name, img_extension = return_file_name(img_filepath)
+    print(f"kuvan nimi: {img_name}")
+    wm_image_path = "images_with_text/" + img_name + wm_text + img_extension
+    img_filepath = wm_image_path
+    watermark_image.save(wm_image_path)
+    open_image(wm_image_path)
+
 
 def open_image(image_path):
     # Open an image using Pillow (JPG reads as <class 'PIL.JpegImagePlugin.JpegImageFile'>)
@@ -47,22 +106,46 @@ def open_image(image_path):
     # Create a label to display the image
     image_label.configure(image=image_tk)
     image_label.image = image_tk
-def upload_action(event=None):
-    global filepath_var
-    filepath = filedialog.askopenfilename()
-    print("Selected:", filepath)
-    print("Which type is:", type(filepath))
-    filepath_var.set(filepath)
-    open_image(filepath)
+def upload_image():
+    global img_filepath_var, img_filepath
+    img_filepath = filedialog.askopenfilename()
+    print("Selected:", img_filepath)
+    print("Which type is:", type(img_filepath))
+    img_filepath_var.set(img_filepath)
+    open_image(img_filepath)
 
-# Set button
-add_file_button = tkinter.Button(root, text="Open file", command=upload_action)
-add_file_button.grid(column=3, row=0)
+def upload_logo():
+    global logo_filepath_var, logo_filepath
+    logo_filepath = filedialog.askopenfilename()
+    print("Selected:", logo_filepath)
+    print("Which type is:", type(logo_filepath))
+    logo_name, logo_extension = return_file_name(logo_filepath)
+    logo_filepath_var.set(logo_name + logo_extension)
+
+# Set buttons
+add_file_button = tkinter.Button(mainframe, width=8, text="Open file", command=upload_image)
+add_file_button.grid(column=2, row=0, sticky=W, padx=2)
+
+add_text_button = tkinter.Button(mainframe, width=8, text="Add text", command=add_text_on_image)
+add_text_button.grid(column=2, row=1, sticky=W, padx=2)
+
+open_logo_button = tkinter.Button(mainframe, width=8, text="Open logo", command=upload_logo)
+open_logo_button.grid(column=2, row=2, sticky=W, padx=2)
+
+add_logo_button = tkinter.Button(mainframe, width=8, text="Add logo", command=add_logo_on_image)
+add_logo_button.grid(column=3, row=2, padx=2)
 
 # Set labels
-ttk.Label(mainframe, text="Filename").grid(column=1, row=0, padx=(0,10))
-ttk.Label(mainframe, textvariable=filepath_var).grid(column=2, row=0)
+ttk.Label(mainframe, text="Filename: ").grid(column=0, row=0, sticky=E)
+ttk.Label(mainframe, textvariable=img_filepath_var).grid(column=1, row=0, sticky=W)
 
+ttk.Label(mainframe, text="Watermark text:").grid(column=0, row=1)
 
+ttk.Label(mainframe, text="Logo file: ").grid(column=0, row=2, sticky=E)
+ttk.Label(mainframe, textvariable=logo_filepath_var).grid(column=1, row=2, sticky=W)
+
+# Text entry fields
+text_input = Entry(mainframe, width=70)
+text_input.grid(column=1, row=1)
 
 root.mainloop()
